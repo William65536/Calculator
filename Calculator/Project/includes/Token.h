981f0e6e467ee5dff335058606ce8da28e8ea621
William65536 <110579495+William65536@users.noreply.github.com>
2023-03-07 22:56:27 -0500

    Update Token.h

--- Calculator/Project/includes/Token.h
+++ Calculator/Project/includes/Token.h
@@ -1,6 +1,11 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+#include <stddef.h>
+#include <stdbool.h>
+
+#define TOKEN_DEFAULT_CAPACITY 0
+
 typedef enum {
     TOKEN_TYPE_NONE,
     TOKEN_TYPE_NUMBER,
@@ -9,6 +14,23 @@ typedef enum {
     TOKEN_TYPE_RPAREN
 } TokenType;
 
+typedef struct {
+    TokenType type;
+    union {
+        double number;
+        double (*operator)(double, double);
+    };
+    size_t col;
+} Token;
+
 typedef struct TokenList TokenList;
+
+TokenList *TokenList_new(size_t initcap);
+
+void TokenList_delete(TokenList **list);
+
+bool TokenList_push(TokenList **list, Token token);
+
+void TokenList_clear(TokenList *list);
 
 #endif
