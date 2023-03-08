0da9e74f2e0c423cde23b940aae57bebdf098fa6
William65536 <110579495+William65536@users.noreply.github.com>
2023-03-07 23:01:58 -0500

    Update main.c

--- Calculator/Project/main.c
+++ Calculator/Project/main.c
@@ -4,26 +4,9 @@
 int main(void)
 {
     String str = String_new(STRING_DEFAULT_CAPACITY);
-    String_push(&str, 'a');
-    String_push(&str, 'p');
-    String_push(&str, 'p');
-    String_push(&str, 'l');
-    String_push(&str, 'e');
-    String_push(&str, 's');
-    String_push(&str, 'a');
-    String_push(&str, 'u');
-    String_push(&str, 'c');
-    String_push(&str, 'e');
-    String_push(&str, ' ');
-    String_push(&str, 'i');
-    String_push(&str, 's');
-    String_push(&str, ' ');
-    String_push(&str, 'y');
-    String_push(&str, 'u');
-    String_push(&str, 'm');
-    String_push(&str, 'm');
-    String_push(&str, 'y');
-    String_push(&str, '!');
+
+    for (size_t i = 0; i < 10000; i++)
+        String_push(&str, 'a');
 
     puts(str);
 
